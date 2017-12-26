3ed341c361dabf3113e14a3d9c4fbf0ea66f3834
Tadas <tadaspaplauskas@gmail.com>
2017-12-26 21:45:09 +0200

    errors in appropriate place

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,24 +6,23 @@
     
     {{ csrf_field() }}
 
-    <label for="email" class="">E-Mail Address</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-
+    @if ($errors->has('password'))
+        <p class="red">
+            <strong>{{ $errors->first('password') }}</strong>
+        </p>
+    @endif
     @if ($errors->has('email'))
         <p class="red">
             <strong>{{ $errors->first('email') }}</strong>
         </p>
     @endif
 
+    <label for="email" class="">E-Mail Address</label>
+    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+
     <label for="password" class="">Password</label>
-        <input id="password" type="password" class="" name="password" required>
-
-        @if ($errors->has('password'))
-            <p class="red">
-                <strong>{{ $errors->first('password') }}</strong>
-            </p>
-        @endif
-
+    <input id="password" type="password" class="" name="password" required>
+    
     <label>
         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
     </label>
