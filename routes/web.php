73c89249d5d3a659301e95b3fdc888cf98b15125
Tadas <tadaspaplauskas@gmail.com>
2017-12-30 18:30:06 +0200

    create Website resource

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'PagesController@index')->name('index');
+
+Route::resource('websites', 'WebsiteController');
