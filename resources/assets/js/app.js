8d36447953eed99289c3745da03b76c77e07ee7e
Tadas <tadaspaplauskas@gmail.com>
2018-01-20 21:20:02 +0200

    deal with turbolinks messing up anchors in urls

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,9 @@
-// var Turbolinks = require('turbolinks');
+var Turbolinks = require('turbolinks');
 
 // Turbolinks.start();
+
+document.addEventListener('turbolinks:click', function (event) {
+    if (event.target.getAttribute('href').charAt(0) === '#') {
+        return event.preventDefault();
+    }
+});
