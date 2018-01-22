42f42453373311172492ff9f879223410517e80d
Tadas <tadaspaplauskas@gmail.com>
2018-01-22 20:49:03 +0200

    disable turbolinks for now, and maybe forever

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,14 +1,14 @@
-Turbolinks = require('turbolinks');
-Turbolinks.start();
+// Turbolinks = require('turbolinks');
+// Turbolinks.start();
 
-document.addEventListener('turbolinks:click', function (event) {
-    if (event.target.getAttribute('href').charAt(0) === '#') {
-        return event.preventDefault();
-    }
-});
+// document.addEventListener('turbolinks:click', function (event) {
+//     if (event.target.getAttribute('href').charAt(0) === '#') {
+//         return event.preventDefault();
+//     }
+// });
 
-function whenReady(fn) {
-    document.addEventListener('turbolinks:load', function (event) {
-        fn();
-    });
-}
+// function whenReady(fn) {
+//     document.addEventListener('turbolinks:load', function (event) {
+//         fn();
+//     });
+// }
