f8ca1f4d4580bf14959ab76f02f375625756e9f7
Tadas <tadaspaplauskas@gmail.com>
2018-03-31 22:29:27 +0300

    leave one file

--- provision.sh
+++ provision.sh
@@ -59,15 +59,53 @@ service apache2 restart
 
 # CONFIGURE MARIADB
 mysql_secure_installation
+
 systemctl restart mysql.service
 systemctl enable mysql.service
 
 # UTILITIES
 echo 'alias ls="ls -halp"' >> ~/.bash_profile
 
+git clone git@github.com:tadaspaplauskas/vimrc.git
 
-# testing web server
+
+# testing web server configuration
 # ab -n 10000 -c 100 -k -l -H "Accept-Encoding: gzip, deflate" http://localhost/login
 
 # finding out how much memory a process is using
 #ps aux | grep 'apache2' | awk '{print $6/1024 "mb";}'
+
+
+#################################################
+# this is unfinished nginx+fpm based provisioning script
+# #!/bin/bash
+
+# # Initial server setup
+# # https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-16-04
+# # Set PasswordAuthentication no
+# vim /etc/ssh/sshd_config
+# systemctl reload sshd
+
+# # Dependencies
+# apt install software-properties-common
+# add-apt-repository ppa:ondrej/php
+# LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/nginx
+
+# apt update
+# apt upgrade
+
+# apt install nginx
+# apt install php7.2-fpm # must install first to avoid installing apache with php
+# apt install php7.2-cli
+# apt install postgres
+# apt install redis
+
+# # Configuration
+# # ttps://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-ubuntu-16-04
+# # https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-in-ubuntu-16-04
+# # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-16-04
+
+# # enable firewall
+# ufw allow 'Nginx HTTP'
+# ufw allow 'OpenSSH'
+# ufw enable
