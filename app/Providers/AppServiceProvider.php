35217c53f62bdb57e3b0819e9226aa709302c140
Tadas <tadaspaplauskas@gmail.com>
2018-03-31 20:19:12 +0300

    fix laravel issue with mariadb

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // fix for mariadb regarding default laravel charset
+        # https://github.com/laravel/docs/blob/5.4/migrations.md#index-lengths--mysql--mariadb
+        Schema::defaultStringLength(191);
     }
 
     /**
--- provision-apache.sh
+++ provision-apache.sh
@@ -14,7 +14,7 @@ LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 
 apt update
 
-apt install apache2 php7.2 libapache2-mod-php7.2 php-pear php7.2-curl php7.2-gd php7.2-mbstring php7.2-zip php7.2-xml mariadb-server mariadb-client
+apt install apache2 php7.2 libapache2-mod-php7.2 php-pear php7.2-curl php7.2-gd php7.2-mbstring php7.2-zip php7.2-xml php7.2-mysql mariadb-server mariadb-client
 
 # enable firewall
 ufw allow 'OpenSSH'
