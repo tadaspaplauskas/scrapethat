0effeb328458f21798d1fb60a50b2258585850e5
Tadas <tadaspaplauskas@gmail.com>
2018-06-19 11:21:56 +0300

    cleanup stuff

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -6,6 +6,7 @@ use Laravel\Dusk\TestCase as BaseTestCase;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Laravel\Dusk\Browser;
 
 abstract class DuskTestCase extends BaseTestCase
 {
@@ -41,17 +42,13 @@ abstract class DuskTestCase extends BaseTestCase
         );
     }
 
-    protected function tearDown()
-    {
-        parent::tearDown();
-
-        static::$browsers->first()->driver->manage()->deleteAllCookies();
-    }
-
     protected function setUp()
     {
         parent::setUp();
 
-        $this->seed(\DatabaseSeeder::class);
+        // reset session before the test
+        $this->browse(function (Browser $browser) {
+            $browser->driver->manage()->deleteAllCookies();
+        });
     }
 }
