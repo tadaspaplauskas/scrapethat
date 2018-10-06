8276734f33da30315d2fe96ebfbc25a97759dea5
Tadas <tadas@paplauskas.lt>
2018-10-06 15:40:37 +0300

    scheduled refresh

---
+++ app/Console/Commands/RefreshSnapshots.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use App\Models\Snapshot;
+
+class RefreshSnapshots extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'snapshot:refresh:daily';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Refresh snapshots daily';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $yesterday = Carbon::now()->subDay();
+
+        Snapshot::where('updated_at', '<', $yesterday)
+            ->get()
+            ->each(function ($snapshot) {
+                $snapshot->download();
+            });
+    }
+}
