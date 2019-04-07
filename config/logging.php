1a69d50037e18e967fe6c20d3976265f34523ab2
Tadas <tadas@paplauskas.lt>
2019-04-07 13:11:18 +0300

    log everything to a single file

--- config/logging.php
+++ config/logging.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'daily'),
+    'default' => env('LOG_CHANNEL', 'single'),
 
     /*
     |--------------------------------------------------------------------------
